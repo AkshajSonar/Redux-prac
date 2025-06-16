b60fd2f240c56d858ede85d235747dd3ecef1117
AkshajSonar <akshaj.sonar@gmail.com>
2025-06-16 20:12:31 +0530

    added the rest and input field

--- redux-prac/src/components/counter.jsx
+++ redux-prac/src/components/counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../redux/counterSlice.js';
+import { increment, decrement,reset,differby } from '../redux/counterSlice.js';
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
@@ -12,6 +12,22 @@ const Counter = () => {
       <h2>{count}</h2>
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())} style={{ marginLeft: '10px' }}>-</button>
+      <button onClick={() => dispatch(reset())} style={{ marginLeft: '10px' }}>Reset</button>
+      <input
+        type="number"
+        placeholder="Enter a number"
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            const value = parseInt(e.target.value, 10);
+            if (!isNaN(value)) {
+              dispatch(differby(value));
+            }
+            e.target.value = ''; // Clear the input field
+          }
+        }}
+        style={{ marginLeft: '10px', width: '100px' }}
+      />
+      
     </div>
   );
 };
--- redux-prac/src/redux/counterSlice.js
+++ redux-prac/src/redux/counterSlice.js
@@ -9,11 +9,16 @@ const counterSlice = createSlice({
     },
     decrement: (state) => {
       state.value -= 1;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
+    differby: (state,action) =>{
+      state.value += action.payload;
     }
-  }
-});
+}});
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement,reset,differby } = counterSlice.actions;
 export default counterSlice.reducer;
 
 // slices are the features and reducers are the functions that handle the state changes
